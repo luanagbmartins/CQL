55d44cc597bb103d97b75a4ae5e457122841422e
luanagbmartins <luanagbmartins@gmail.com>
2021-01-25 22:23:19 -0300

    fix: custom env

--- atari/train.sh
+++ atari/train.sh
@@ -1,1 +1,9 @@
-python -m batch_rl.fixed_replay.train
+DATASET_NAME="v5_scoremax_dataset_cql"
+
+
+mkdir -p cql-dataset/${DATASET_NAME}/
+gsutil -m rsync -r gs://ac-rl-artifacts/data/processed/cql-dataset/${DATASET_NAME} cql-dataset/${DATASET_NAME}
+
+
+python -m batch_rl.fixed_replay.train \
+--gin-bindings "['FixedReplayRunner.num_iterations=1000', 'FixedReplayQuantileAgent.minq_weight=4.0']"
